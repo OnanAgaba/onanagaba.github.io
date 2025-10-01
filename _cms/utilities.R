e919c5019251db35ea227a499e645c8a310a2f93
OnanAgaba <97107076+OnanAgaba@users.noreply.github.com>
2025-10-01 18:37:18 +0300

    Delete _cms directory

--- _cms/utilities.R
+++
@@ -1,8 +0,0 @@
-# general support utilities
-
-# read an entire text file
-slurpFile <- function(fileName) readChar(fileName, file.info(fileName)$size)
-
-# missing value handling
-is.absent <- function(x) is.null(x) || is.na(x[1])
-naToNull <- function(x) if(is.na(x)) NULL else x
